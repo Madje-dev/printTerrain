f268597b5052632cfa181c5035eb6eb67d0756fc
madjeist <dhurian@outlook.com>
2025-10-06 19:54:08 +0200

    I am able to read from and write to a file

--- main.cpp
+++ main.cpp
@@ -1,31 +1,63 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include "main.h"
+#include <cstdlib>
+#include <vector>
 
 int main(){
-    int countRow, countColumn;
+    int countRow, countColumn, count;
+    int countArrayColumn, countArrayRow;
     float row, column, heigth;
     float maxRow, maxColumn;
     float incrementRow, increamentColumn;
-    std::string myText;
-    std::ifstream MyReadFile("coordinates.dat");
+    std::string myText, parsedCoordinates;
+    std::stringstream auxString; 
+    std::vector<std::vector<float>> aPos;
+    std::vector<float> tempArray;
 
-//    WriteCoordinatesFile(row, column, heigth, maxRow, maxColumn, incrementRow, increamentColumn, countRow, countColumn);
+  
 
-    while(std::getline(MyReadFile, myText)){
-        std::cout<<myText<< "\n "<<std::endl;
-        
-    }
+    //WriteCoordinatesFile(row, column, heigth, maxRow, maxColumn, incrementRow, increamentColumn, countRow, countColumn);
+      std::ifstream MyReadFile("coordinates.dat");
+      ReadCoordinates(countArrayRow, MyReadFile, myText, countArrayColumn, auxString, parsedCoordinates, tempArray, aPos);
 
-
-    return 0;
+      return 0;
 }
 
+void ReadCoordinates(int &countArrayRow, std::ifstream &MyReadFile, std::string &myText, int &countArrayColumn, std::stringstream &auxString, std::string &parsedCoordinates, std::vector<float> &tempArray, std::vector<std::vector<float>> &aPos)
+{
+    countArrayRow = 0;
+    while (std::getline(MyReadFile, myText, ' '))
+    {
+        // std::cout<<myText<< "\n ";
+        countArrayColumn = 0;
+        auxString << myText;
+        while (std::getline(auxString, parsedCoordinates, ','))
+        {
 
+            tempArray.push_back(std::stof(parsedCoordinates));
+        }
+        aPos.push_back(tempArray);
+
+        tempArray.clear();
+        auxString.clear();
+    }
+
+    for (const auto &row1 : aPos)
+    {
+        for (float val : row1)
+        {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+}
 
 void WriteCoordinatesFile(float &row, float &column, float &heigth, float &maxRow, float &maxColumn, float &incrementRow, float &increamentColumn, int &countRow, int &countColumn)
 {
+    float delta=0.001;
     std::string sentence;
     std::ofstream Myfile("coordinates.dat");
     row = 0;
@@ -37,22 +69,22 @@ void WriteCoordinatesFile(float &row, float &column, float &heigth, float &maxRo
     increamentColumn = 0.1;
     countRow = 0;
     countColumn = 0;
-    for (row = maxRow; row >= -0.1; row = row - incrementRow)
+    
+    for (row = maxRow; row >= -delta; row = row - incrementRow)
     {
         sentence = "";
         countColumn = 0;
         for (column = 0; column <= maxColumn; column = column + increamentColumn)
         {
 
-            sentence = sentence + std::to_string(row) + "," + std::to_string(column) + "," + std::to_string(heigth) + " ,";
+            sentence = sentence + std::to_string(row) + "," + std::to_string(column) + "," + std::to_string(heigth) + ", ";
             countColumn++;
         };
-        sentence = sentence + "\n";
+        sentence = sentence + " \n";
         std::cout << sentence << std::endl;
 
         Myfile << sentence;
         countRow++;
     };
-    Myfile << countRow << "," << countColumn;
     Myfile.close();
 }
--- main.h
+++ main.h
@@ -1,2 +1,5 @@
 #pragma once
+#include <vector>
 void WriteCoordinatesFile(float &row, float &column, float &heigth, float &maxRow, float &maxColumn, float &incrementRow, float &increamentColumn, int &countRow, int &countColumn);
+
+void ReadCoordinates(int &countArrayRow, std::ifstream &MyReadFile, std::string &myText, int &countArrayColumn, std::stringstream &auxString, std::string &parsedCoordinates, std::vector<float> &tempArray, std::vector<std::vector<float>> &aPos);
